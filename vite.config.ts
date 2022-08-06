e09a90eae5fc27ca4d04699d3b88d79e6d9054e5
lruler <1204312199@qq.com>
2022-08-06 16:56:44 +0800

    [feat]: 配置路径别名

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,23 @@
 import { defineConfig } from 'vite';
+import path from 'path';
 import react from '@vitejs/plugin-react';
 import tsconfigPaths from 'vite-tsconfig-paths';
 import autoRoute from './src/plugins/vite-plugin-react-autoroutes';
 
 export default defineConfig({
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, 'src'),
+      components: path.resolve(__dirname, 'src/components'),
+      styles: path.resolve(__dirname, 'src/styles'),
+      plugins: path.resolve(__dirname, 'src/plugins'),
+      views: path.resolve(__dirname, 'src/views'),
+      layouts: path.resolve(__dirname, 'src/layouts'),
+      utils: path.resolve(__dirname, 'src/utils'),
+      apis: path.resolve(__dirname, 'src/apis'),
+      dirs: path.resolve(__dirname, 'src/directives'),
+    },
+  },
   plugins: [
     tsconfigPaths(),
     react({
