b7f1d61c72196ce2c07b3dbf4edabd5c9b8a6c10
lruler <1204312199@qq.com>
2022-10-21 00:21:05 +0800

    fix: 适配Js控制剪切板

--- src/pages/Article/[article_id].tsx
+++ src/pages/Article/[article_id].tsx
@@ -74,8 +74,27 @@ const Icon: React.FC<{ onClick?: () => void; type: string }> = ({ type, onClick 
     <Action
       onClick={() => {
         if (type.includes('share')) {
-          navigator.clipboard.writeText(location.href);
-          message.success('文章链接已复制到剪切板上!');
+          if (navigator.clipboard) {
+            // clipboard api 复制
+            navigator.clipboard.writeText(location.href);
+            message.success('文章链接已复制到剪切板上!');
+          } else {
+            var textarea = document.createElement('textarea');
+            document.body.appendChild(textarea);
+            // 隐藏此输入框
+            textarea.style.position = 'fixed';
+            textarea.style.clip = 'rect(0 0 0 0)';
+            textarea.style.top = '10px';
+            // 赋值
+            textarea.value = location.href;
+            // 选中
+            textarea.select();
+            // 复制
+            document.execCommand('copy', true);
+            // 移除输入框
+            document.body.removeChild(textarea);
+            message.success('文章链接已复制到剪切板上!');
+          }
         } else {
           onClick && onClick();
         }
